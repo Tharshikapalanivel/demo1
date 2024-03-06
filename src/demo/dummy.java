6ed0b37a75d22f24a61e142d48368e9f526a7c0f
tharshika.p <tharshika.p@zohocorp.com>
2024-03-06 10:47:58 +0530

    second commit

--- src/demo/dummy.java
+++ src/demo/dummy.java
@@ -5,7 +5,7 @@ public class dummy {
         if(System.out.append("school ")==null){
             System.out.println("school ");
         }else{
-            System.out.println("bus");
+            System.out.println("buses");
         }
     }
 }
