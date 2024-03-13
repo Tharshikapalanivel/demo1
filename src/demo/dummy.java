fa226182a3d0853b8756277cf101bea74231e1c9
tharshika.p <tharshika.p@zohocorp.com>
2024-03-13 08:30:35 +0530

    new changes done

--- src/demo/dummy.java
+++ src/demo/dummy.java
@@ -5,7 +5,7 @@ public class dummy {
         if(System.out.append("school ")==null){
             System.out.println("school ");
         }else{
-            System.out.println("buses");
+            System.out.println("buses are wonderful");
         }
     }
 }
